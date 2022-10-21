dc7db46b3b52f5e36f6749d2198bc6f0a8ffac98
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2022-10-21 15:47:10 +0300

    CookieModal Edit

--- src/components/Cookies/CookieModal.vue
+++ src/components/Cookies/CookieModal.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from "vue";
 import { getLocalCookie, saveLocalCookie } from "../../service/local";
-
+import CookieSettings from "./CookieSettings.vue";
 const cookie = ref(null);
+const isOpen = ref(false);
 
 const props = defineProps({
 	rejectButtonActive: {
@@ -11,7 +12,7 @@ const props = defineProps({
 	},
 	cookieModalBGC: {
 		type: String,
-		default: "rgb(38, 38, 38)"
+		default: "rgb(38, 38, 38)",
 	},
 	cookieHeader: {
 		type: String,
@@ -19,7 +20,7 @@ const props = defineProps({
 	},
 	cookieHeaderColor: {
 		type: String,
-		default: "white"
+		default: "white",
 	},
 	cookieDescription: {
 		type: String,
@@ -28,7 +29,7 @@ const props = defineProps({
 	},
 	cookieDescriptionColor: {
 		type: String,
-		default: "white"
+		default: "white",
 	},
 	cookiePolicy: {
 		type: String,
@@ -36,7 +37,7 @@ const props = defineProps({
 	},
 	cookiePolicyColor: {
 		type: String,
-		default: "orange"
+		default: "orange",
 	},
 	cookieSettings: {
 		type: String,
@@ -53,27 +54,27 @@ const props = defineProps({
 	// },
 	acceptButtonText: {
 		type: String,
-		default: "Kabul Et"
+		default: "Kabul Et",
 	},
 	acceptButtonColor: {
 		type: String,
-		default: "white"
+		default: "white",
 	},
 	acceptButtonBGC: {
 		type: String,
-		default: "rgb(255, 128, 0)"
+		default: "rgb(255, 128, 0)",
 	},
 	declineButtonText: {
 		type: String,
-		default: "Reddet"
+		default: "Reddet",
 	},
 	declineButtonColor: {
 		type: String,
-		default: "white"
+		default: "white",
 	},
 	declineButtonBGC: {
 		type: String,
-		default: "rgb(255, 128, 0)"
+		default: "rgb(255, 128, 0)",
 	},
 	// declineButton: {
 	// 	text: {
@@ -85,7 +86,6 @@ const props = defineProps({
 	// 		default: "rgb(255 96 0 / var(--tw-bg-opacity))"
 	// 	}
 	// }
-
 });
 
 onMounted(() => {
@@ -105,30 +105,64 @@ function declineCookie() {
 	console.log(cookie.value);
 }
 
+function openSettings(){
+	isOpen.value = true;
+}
+
+function closeModalHandler(isClosed){
+ isOpen.value = isClosed.value;
+}
+
 </script>
 
 <template>
-
+	<div v-if="isOpen">
+		<CookieSettings :isOpen="isOpen" @closeModal="closeModalHandler"></CookieSettings>
+	</div>
 	<template v-if="!cookie">
-		<div class="cookie" :style="{backgroundColor: props.cookieModalBGC}">
+		<div class="cookie" :style="{ backgroundColor: props.cookieModalBGC }">
 			<div class="cookie__description">
 				<div class="cookie__header">
-					<p :style="{color: props.cookieHeaderColor}">{{props.cookieHeader}}</p>
+					<p :style="{ color: props.cookieHeaderColor }">
+						{{ props.cookieHeader }}
+					</p>
 				</div>
 				<div class="cookie__text">
-					<p :style="{color: props.cookieDescriptionColor}">{{props.cookieDescription}}
-						<a class="policy" :style="{color: props.cookiePolicyColor}" href="#">{{props.cookiePolicy}}</a>
+					<p :style="{ color: props.cookieDescriptionColor }">
+						{{ props.cookieDescription }}
+						<a
+							class="policy"
+							:style="{ color: props.cookiePolicyColor }"
+							href="#"
+							>{{ props.cookiePolicy }}</a
+						>
 					</p>
 				</div>
 			</div>
 			<div class="cookie__buttons">
-				<button @click="acceptCookie" class="accept-cookie"
-					:style="{backgroundColor: props.acceptButtonBGC, color: props.acceptButtonColor}">{{props.acceptButtonText}}</button>
+				<button
+					@click="acceptCookie"
+					class="accept-cookie"
+					:style="{
+						backgroundColor: props.acceptButtonBGC,
+						color: props.acceptButtonColor,
+					}"
+				>
+					{{ props.acceptButtonText }}
+				</button>
 				<template v-if="props.rejectButtonActive">
-					<button @click="declineCookie" class="decline-cookie"
-						:style="{backgroundColor: props.declineButtonBGC, color: props.declineButtonColor}">{{props.declineButtonText}}</button>
+					<button
+						@click="declineCookie"
+						class="decline-cookie"
+						:style="{
+							backgroundColor: props.declineButtonBGC,
+							color: props.declineButtonColor,
+						}"
+					>
+						{{ props.declineButtonText }}
+					</button>
 				</template>
-				<a class="cookie-settings" href="#">{{props.cookieSettings}}</a>
+				<a class="cookie-settings" @click="openSettings">{{props.cookieSettings}}</a>
 			</div>
 		</div>
 	</template>
@@ -140,7 +174,6 @@ function declineCookie() {
 	box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
 
 	&__description {
-
 		@apply w-full flex justify-center flex-col p-3;
 
 		.cookie__header {
@@ -170,7 +203,6 @@ function declineCookie() {
 
 		.accept-cookie:hover {
 			@apply text-gray-300 brightness-90;
-
 		}
 
 		.decline-cookie {
@@ -180,16 +212,11 @@ function declineCookie() {
 
 		.decline-cookie:hover {
 			@apply text-gray-300 brightness-90;
-
 		}
 
 		.cookie-settings:hover {
 			@apply brightness-75;
 		}
 	}
-
-
-
-
 }
 </style>
--- src/components/Cookies/CookieSettings.vue
+++ src/components/Cookies/CookieSettings.vue
@@ -1,13 +1,170 @@
 <script setup>
+import { ref } from "vue";
+const props = defineProps(['isOpen']);
+const emits = defineEmits(["closeModal"]);
+
+const isModalOpen = ref(false);
+
+const closeModal = () =>{
+	emits("closeModal", isModalOpen)
+};
 
 
 </script>
 
 <template>
+	<div class="modal">
+		<button class="close-modal" @click="closeModal">&times;</button>
+		<h1 class="modal--title">Çerez Ayarları</h1>
 
-
+		<table class="cookie--table">
+			<tr>
+				<td>
+                    <p>Zorunlu çerezler</p>
+                </td>
+				<td>
+					<label class="switch">
+						<input type="checkbox" checked />
+						<span class="slider round"></span>
+					</label>
+				</td>
+			</tr>
+			<tr>
+				<td>
+                    <p>Performans Çerezleri</p>
+                </td>
+				<td>
+					<label class="switch">
+						<input type="checkbox" />
+						<span class="slider round"></span>
+					</label>
+				</td>
+			</tr>
+			<tr>
+				<td>
+                    <p>Hedefleme Çerezleri</p>
+                </td>
+				<td>
+					<label class="switch">
+						<input type="checkbox" />
+						<span class="slider round"></span>
+					</label>
+				</td>
+			</tr>
+			<tr>
+				<td>
+                    <p>İşlevsellik Çerezleri</p>
+                </td>
+				<td>
+					<label class="switch">
+						<input type="checkbox" />
+						<span class="slider round"></span>
+					</label>
+				</td>
+			</tr>
+		</table>
+	</div>
 </template>
 
 <style scoped>
+.modal {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	width: 35%;
 
+	background-color: white;
+	padding: 2rem;
+	border-radius: 7px;
+	box-shadow: 0 3rem 5rem rgba(0, 0, 0, 0.3);
+	z-index: 10;
+}
+.modal--title{
+    text-align: center;
+}
+
+.close-modal {
+	position: absolute;
+	top: -1.8rem;
+	right: 1rem;
+	font-size: 4rem;
+	color: #333;
+	cursor: pointer;
+	border: none;
+	background: none;
+}
+
+h1 {
+	font-size: 2.5rem;
+	margin-bottom: 2rem;
+}
+
+p {
+	font-size: 1.8rem;
+}
+
+.cookie--table{
+    width :100%;
+}
+
+.switch {
+	position: relative;
+	display: inline-block;
+	width: 60px;
+	height: 34px;
+}
+
+.switch input {
+	opacity: 0;
+	width: 0;
+	height: 0;
+}
+
+.slider {
+	position: absolute;
+	cursor: pointer;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background-color: #ccc;
+	-webkit-transition: 0.4s;
+	transition: 0.4s;
+}
+
+.slider:before {
+	position: absolute;
+	content: "";
+	height: 26px;
+	width: 26px;
+	left: 4px;
+	bottom: 4px;
+	background-color: white;
+	-webkit-transition: 0.4s;
+	transition: 0.4s;
+}
+
+input:checked + .slider {
+	background-color: green;
+}
+
+input:focus + .slider {
+	box-shadow: 0 0 1px green;
+}
+
+input:checked + .slider:before {
+	-webkit-transform: translateX(26px);
+	-ms-transform: translateX(26px);
+	transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+	border-radius: 34px;
+}
+
+.slider.round:before {
+	border-radius: 50%;
+}
 </style>
